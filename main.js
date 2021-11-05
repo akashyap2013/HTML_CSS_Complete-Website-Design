96f00e1a469e39864e8e6161955f4b9163839313
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2021-11-05 16:39:41 +0530

    Navigation Menu Added

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+// execute script after page load
+window.onload = function digital_fn(){
+
+    // toggle button
+    let toggle = document.querySelector("#nav .toggle-btn");
+    let collapse = document.querySelector("#nav .collapse");
+
+    toggle.addEventListener('click', function(event){
+        collapse.classList.toggle('active')
+        // console.log(toggle)
+    });
+
+}
